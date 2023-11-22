aad386be1612c211cae03099c4c03a36e6baaf81
Karolina Romanowska <k.romanowska@stud.elka.pw.edu.pl>
2023-11-22 01:05:18 +0100

    add metric learning

--- models/MetricLearningLoss.py
+++ models/MetricLearningLoss.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+
 class MetricLearningLoss(nn.Module):
     def __init__(self, sigma=0.2, omega=1.0):
         super(MetricLearningLoss, self).__init__()
@@ -16,14 +17,14 @@ class MetricLearningLoss(nn.Module):
         for i in range(len(labels)):
             for j in range(i + 1, len(labels)):
                 # Calculate Euclidean distance squared between outputs[i] and outputs[j]
-                distance_squared = torch.sum((outputs[i] - outputs[j])**2)
+                distance_squared = torch.sum((outputs[i] - outputs[j]) ** 2)
 
                 # Check if the pair belongs to the same class or different classes
                 if labels[i] == labels[j]:
                     loss += -(k / 2 - 1) * torch.log(distance_squared / (2 * k) / (self.sigma ** 2)) + 0.5 * (
-                                distance_squared / (2 * k) / (self.sigma ** 2))
+                            distance_squared / (2 * k) / (self.sigma ** 2))
                 else:
                     loss += (k / 2 - 1) * torch.log(distance_squared / (2 * k) / (self.omega ** 2)) - 0.5 * (
-                                distance_squared / (2 * k) / (self.omega ** 2))
+                            distance_squared / (2 * k) / (self.omega ** 2))
 
         return loss
