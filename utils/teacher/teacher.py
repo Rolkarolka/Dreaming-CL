83627a74fd9689ccd464d36ac642af4ba1a6c80f
Karolina Romanowska <k.romanowska@stud.elka.pw.edu.pl>
2024-01-31 10:19:40 +0100

    reformat

--- utils/teacher/teacher.py
+++ utils/teacher/teacher.py
@@ -166,58 +166,58 @@ def test(epoch):
 classes = ('plane', 'car', 'bird', 'cat', 'deer',
            'dog', 'frog', 'horse', 'ship', 'truck')"""
 
-lr = 0.1
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-best_acc = 0  # best test accuracy
-num_epochs = 200
-batch_size = 64
-classes_to_learn = [0, 1, 2]
+if __name__ == '__main__':
+    lr = 0.1
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    best_acc = 0  # best test accuracy
+    num_epochs = 200
+    batch_size = 64
+    classes_to_learn = [0, 1, 2]
 
-# Data
-print('==> Preparing data..')
-transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
+    # Data
+    print('==> Preparing data..')
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
 
-transform_test = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
 
-train_set = torchvision.datasets.CIFAR10(
-    root='./data', train=True, download=True, transform=transform_train)
-train_indices = [i for i in range(len(train_set)) if train_set[i][1] in classes_to_learn]
+    train_set = torchvision.datasets.CIFAR10(
+        root='./data', train=True, download=True, transform=transform_train)
+    train_indices = [i for i in range(len(train_set)) if train_set[i][1] in classes_to_learn]
 
-trainloader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, num_workers=2,
-                                          sampler=SubsetRandomSampler(train_indices))
+    trainloader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, num_workers=2,
+                                              sampler=SubsetRandomSampler(train_indices))
 
-test_set = torchvision.datasets.CIFAR10(
-    root='./data', train=False, download=True, transform=transform_test)
-test_indices = [i for i in range(len(test_set)) if test_set[i][1] in classes_to_learn]
-testloader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, num_workers=2,
-                                         sampler=SubsetRandomSampler(test_indices))
+    test_set = torchvision.datasets.CIFAR10(
+        root='./data', train=False, download=True, transform=transform_test)
+    test_indices = [i for i in range(len(test_set)) if test_set[i][1] in classes_to_learn]
+    testloader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, num_workers=2,
+                                             sampler=SubsetRandomSampler(test_indices))
 
-# Model
-print('==> Building model..')
-net = ResNet50()
-net = net.to(device)
-if device == 'cuda':
-    net = torch.nn.DataParallel(net)
-    cudnn.benchmark = True
+    # Model
+    print('==> Building model..')
+    net = ResNet50()
+    net = net.to(device)
+    if device == 'cuda':
+        net = torch.nn.DataParallel(net)
+        cudnn.benchmark = True
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=lr,momentum=0.9, weight_decay=5e-4)
-scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=lr, momentum=0.9, weight_decay=5e-4)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
 
-for epoch in range(num_epochs):
-    train(epoch)
-    test(epoch)
-    scheduler.step()
+    for epoch in range(num_epochs):
+        train(epoch)
+        test(epoch)
+        scheduler.step()
 
-
-print('Finished Training')
-classes = '_'.join([f"{i}" for i in classes_to_learn])
-torch.save(net.state_dict(), f"teacher_new_{net.__class__}_classes_{classes}.weights")
+    print('Finished Training')
+    classes = '_'.join([f"{i}" for i in classes_to_learn])
+    torch.save(net.state_dict(), f"teacher_new_{net.__class__}_classes_{classes}.weights")
